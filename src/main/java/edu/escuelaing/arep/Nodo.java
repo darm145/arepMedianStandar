69316a6adcfb0dcd28fd5f19ff8e5455d2f84662
David Ramirez <david.ramirez-mo@mail.escuelaing.edu.co>
2019-08-14 15:37:14 -0500

    inicio documentacion

--- src/main/java/edu/escuelaing/arep/Nodo.java
+++ src/main/java/edu/escuelaing/arep/Nodo.java
@@ -1,18 +1,39 @@
 package edu.escuelaing.arep;
-
+/**
+ * 
+ * @author David Ramirez
+ * la siguiente clase representa un elemento de la linked list denominado como nodo dentro de sus propiedades contiene el valor y el nodo siguiente
+ */
 public class Nodo{
     private Nodo next;
     private int value;
+    /**
+     * Constructor de la clase nodo, sirve para crear un Nodo 
+     * @param next indica el nodo que va en la posicion inmediatamente siguiente.
+     * @param value indica el valor numerico del elemento.
+     */
     public Nodo(Nodo next,int value){
         this.next=next;
         this.value=value;
     }
+    /**
+     * funcion que retorna el nodo siguiente a este en la linked list
+     * @return Nodo next, el siguiente en la linked list
+     */
     public Nodo getNext(){
         return next;
     }
+    /**
+     * funcion que retorna el valor actual del nodo
+     * @return int valor, valor numerico asignado al nodo
+     */
     public int value(){
         return value;
     }
+    /**
+     * cambia el nodo siguiente a uno nuevo
+     * @param node el nodo a definir como siguiente.
+     */
     public void setNext(Nodo node){
         next=node;
     }
